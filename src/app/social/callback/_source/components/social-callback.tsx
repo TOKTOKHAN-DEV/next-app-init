272b9210905e1a6eda56b0853fc411b7147e7cc2
Panxoat <a20304645@gmail.com>
2025-11-10 16:24:15 +0900

    Fix: eslint

--- src/app/social/callback/_source/components/social-callback.tsx
+++ src/app/social/callback/_source/components/social-callback.tsx
@@ -7,8 +7,10 @@ import { PopupCallback } from './popup-callback'
 
 export const SocialCallback = () => {
   const [isPopup, setIsPopup] = useState(true)
+
   useEffect(() => {
     if (!window.opener) {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
       setIsPopup(false)
     }
   }, [isPopup])
